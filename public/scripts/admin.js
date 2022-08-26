41aec5cd73271926669d620ea550124265a2eaf1
30namajnoon <sina.majnoonhjk@gmail.com>
2022-08-26 06:20:32 +0200

    1

--- public/scripts/admin.js
+++ public/scripts/admin.js
@@ -3,7 +3,7 @@
 //     elements       //
 ////////////////////////
 ////////////////////////
-let meno_ekle,menolar;
+let meno_ekle,menolar,urun_ekle,urunler;
 
 
 ////////////////////////
@@ -13,10 +13,11 @@ let meno_ekle,menolar;
 ////////////////////////
 let socket = io();
 let menolar_data = [];
+let urunler_data = [];
 
 
 // socket.emit("data_save","menolar",JSON.stringify(meno_ekle));
-socket.emit("data_load","menolar");
+
 socket.on("data_load",(database,data)=> {
     if (database == "menolar") {
         menolar_data = JSON.parse(data);
@@ -24,6 +25,13 @@ socket.on("data_load",(database,data)=> {
         paszamine_s.innerHTML = "";
         meno_ekle = new MenoEkle();
         menolar = new Menolar();
+    }
+    if (database == "urunler") {
+        urunler_data = JSON.parse(data);
+        console.log(urunler_data);
+        paszamine_s.innerHTML = "";
+        urun_ekle = new UrunEkle();
+        urunler = new Urunler();
     }
 })
 
@@ -51,7 +59,12 @@ function CratePaszamine() {
 ////////////////////////
 ////////////////////////
 function ID_ara(element) {
-    let id = Number(element[element.length-1].id)+1;
+    let id;
+    if(element.length > 0) {
+        id = Number(element[element.length-1].id)+1;
+    }else {
+        id = 1;
+    }
     return id;
 }
 let colors = {
@@ -111,6 +124,9 @@ function NavarAbzar() {
     this.users = Abzar("group");
     this.kasa = Abzar("currency_exchange");
     this.qrcodes = Abzar("qr_code_2");
+    this.meno_add.addEventListener("click",()=> {
+        socket.emit("data_load","menolar");
+    })
     this.Crate();
 }
 NavarAbzar.prototype.Crate = function() {
@@ -144,6 +160,7 @@ function MenoEkle () {
     this.m_add_paszamine_s.style.cssText = "position: relative;float: left;width: 90%;height: 40vw;background-color: #adebf000;margin-left: 5%;margin-top: 20%;",
     this.text = CrateElement("input","","m_add_text","","text");
     this.text.style.cssText = " position: absolute;font-size: 5vw;width: 98%;height: 12vw;border: solid .5vw "+colors.c_4+";padding: .2vw;background-color: "+colors.c_1+";color: "+colors.c_3+";top: 14vw;";
+    this.text.setAttribute("placeholder","Meno Name:........");
     this.text.setAttribute("maxlength","20");
     this.file = CrateElement("input","","add_meno_file","","file");
     this.lable = CrateElement("lable","","m_add_lable");
@@ -193,8 +210,8 @@ MenoEkle.prototype.Crate = function() {
     this.m_add_paszamine_s.appendChild(this.button);
     this.lable.appendChild(this.icon);
 }
-meno_ekle = new MenoEkle();
-function Meno(name_,img_) {
+function Meno(id_,name_,img_) {
+    this.id = id_;
     this.paszamine = CrateElement("div","","meno_div");
     this.img = CrateElement("img","","meno_img");
     this.img.src = img_;
@@ -205,7 +222,8 @@ function Meno(name_,img_) {
     this.paszamine.appendChild(this.h1_div);
     this.h1_div.appendChild(this.h1);
     this.paszamine.addEventListener("click",()=> {
-        console.log("meno");
+        localStorage.setItem("meno_id",this.id);
+        socket.emit("data_load","urunler");
     })
 }
 function Menolar() {
@@ -213,14 +231,163 @@ function Menolar() {
     
     this.meno = [];
     menolar_data.forEach(e => {
-        this.meno.push(new Meno(e.meno_name,e.img));
+        this.meno.push(new Meno(e.id,e.meno_name,e.img));
     });
     this.Crate();
+    this.meno.forEach(e => {
+        e.paszamine.style.height = "40vw";
+        e.h1_div.style.top = ((e.paszamine.getBoundingClientRect().height/2)-(e.h1_div.getBoundingClientRect().height/2))+"px";
+    });
     this.paszamine.style.cssText = " width: 100%;margin-top: "+(meno_ekle.ekle_icon.getBoundingClientRect().y+meno_ekle.ekle_icon.getBoundingClientRect().height)*1.3+"px;overflow-y: scroll;"
     this.paszamine.style.height = innerHeight-this.paszamine.getBoundingClientRect().y+"px";
     
 }
 Menolar.prototype.Crate = function() {
+    paszamine_s.appendChild(this.paszamine);
+    this.meno.forEach(element => {
+        this.paszamine.appendChild(element.paszamine);
+        
+    });
+}
+
+///////////////////////
+////////////////////////
+//      urunler       //
+////////////////////////
+////////////////////////
+function UrunEkle () {
+    this.ekle_icon = CrateElement("span","add_circle","","material-symbols-rounded");
+    this.ekle_icon.style.cssText = "top: 2%;position: absolute;font-size: "+AndazeBaraks(15,15)+"px;color: "+colors.c_4+"";
+    this.m_add_paszamine = CrateElement("div","","m_add_paszamine");
+    this.m_add_paszamine.style.cssText = "display: none;position: relative;width: 100%;height: 100%;background-color: rgba(255, 255, 255, 0.549);"
+    this.m_add_paszamine_s = CrateElement("form","","m_add_paszamine_s");
+    this.m_add_paszamine_s.style.cssText = "position: relative;float: left;width: 90%;height: auto;background-color: #adebf000;margin-left: 5%;margin-top: 20%;",
+    ////// urun adi
+    this.text = CrateElement("input","","m_add_text","","text");
+    this.text.style.cssText = "font-size: 5vw;width: 98%;height: 12vw;border: solid .5vw "+colors.c_4+";padding: .2vw;background-color: "+colors.c_1+";color: "+colors.c_3+";margin-top: 14vw;";
+    this.text.setAttribute("placeholder","Prodoct Name:........");
+    this.text.setAttribute("maxlength","20");
+    ///// urun aciklama
+    this.text_aciklama = CrateElement("input","","m_add_text","","text");
+    this.text_aciklama.style.cssText = "font-size: 5vw;width: 98%;height: 12vw;border: solid .5vw "+colors.c_4+";padding: .2vw;background-color: "+colors.c_1+";color: "+colors.c_3+";margin-top: 1vw;";
+    this.text_aciklama.setAttribute("placeholder","Prodoct Description:.....");
+    ///// urun fiyat
+    this.text_fiyat = CrateElement("input","","m_add_text","","text");
+    this.text_fiyat.style.cssText = "font-size: 5vw;width: 98%;height: 12vw;border: solid .5vw "+colors.c_4+";padding: .2vw;background-color: "+colors.c_1+";color: "+colors.c_3+";margin-top: 1vw;";
+    this.text_fiyat.setAttribute("placeholder","Price of the ptodoct:..");
+
+    this.file = CrateElement("input","","add_meno_file","","file");
+    this.lable = CrateElement("lable","","m_add_lable");
+    this.lable.setAttribute("for","add_meno_file");
+    this.lable.style.cssText = " position: absolute;width: 100%;height: 12vw;background-color: "+colors.c_1+";";
+    this.icon = CrateElement("span","add_photo_alternate","m_add_icon","material-symbols-rounded");
+    this.icon.style.cssText = "font-size: 12vw;color: "+colors.c_4+";margin-left: 40%;";
+    this.button = CrateElement("input","","m_add_button","","button");
+    this.button.value = "save";
+    this.button.style.cssText = "width: 100%;height: 13vw;background-color: "+colors.c_1+";color: "+colors.c_4+";font-size: 6vw;border: solid .5vw "+colors.c_4+";margin-top: 1vw;";
+    
+    this.Crate();
+    this.ekle_icon.style.left = ((paszamine_s.getBoundingClientRect().width/2)-(this.ekle_icon.getBoundingClientRect().width/2))+"px"
+    this.m_add_paszamine_s.style.marginTop = this.ekle_icon.getBoundingClientRect().height*2+"px";
+    this.m_add_paszamine_s.style.height = AndazeBaraks(90,70)-this.ekle_icon.getBoundingClientRect().height*2+"px";
+
+    this.lable.addEventListener("click",(e)=> {
+        this.file.click();
+    })
+    this.m_add_paszamine.addEventListener("touchend",(e)=> {
+        if (e.changedTouches[0].pageY < this.m_add_paszamine_s.getBoundingClientRect().y || e.changedTouches[0].pageY > this.button.getBoundingClientRect().y+this.button.getBoundingClientRect().height*1.5) {
+            this.m_add_paszamine.style.display = "none";
+        }
+    })
+    this.ekle_icon.addEventListener("click",(e)=> {
+        e.stopPropagation();
+        this.m_add_paszamine.style.display = "flex";
+    })
+    this.button.addEventListener("click",(e)=> {
+        e.stopPropagation();
+        let reder = new FileReader();
+        reder.addEventListener("load",()=> {
+            urunler_data.push({id: ID_ara(urunler_data),meno_name: this.text.value,img: reder.result,aciklama: this.text_aciklama.value,fiyat: this.text_fiyat.value,meno_id: localStorage.getItem("meno_id")});
+            socket.emit("data_save","urunler",JSON.stringify(urunler_data));
+            console.log(urunler_data)
+            this.m_add_paszamine.style.display = "none";
+        })
+        reder.readAsDataURL(this.file.files[0]);
+    })
+}
+
+UrunEkle.prototype.Crate = function() {
+    paszamine_s.appendChild(this.ekle_icon);
+    paszamine_s.appendChild(this.m_add_paszamine);
+    this.m_add_paszamine.appendChild(this.m_add_paszamine_s);
+    this.m_add_paszamine_s.appendChild(this.file);
+    this.m_add_paszamine_s.appendChild(this.lable);
+    this.m_add_paszamine_s.appendChild(this.text);
+    this.m_add_paszamine_s.appendChild(this.text_aciklama);
+    this.m_add_paszamine_s.appendChild(this.text_fiyat);
+    this.m_add_paszamine_s.appendChild(this.button);
+    this.lable.appendChild(this.icon);
+}
+function Urun(id_,name_,img_,aciklama_,fiyat_,meno_id_) {
+    this.id = id_;
+    this.name = name_;
+    this.aciklama = aciklama_;
+    this.fiyat = fiyat_;
+    this.meno_id = meno_id_;
+    this.paszamine = CrateElement("div","","meno_div");
+    this.img = CrateElement("img","","meno_img");
+    this.img.src = img_;
+    ///// urun adi
+    
+    this.h1_div = CrateElement("div","","meno_h1_div");
+    this.h1 = CrateElement("h1",name_,"meno_h1");
+    this.h1.style.color = colors.c_3;
+    ///// urun aciklama
+    this.h1_div_aciklama = CrateElement("div","","meno_h1_div");
+    this.h1_aciklama = CrateElement("h1",aciklama_,"meno_h1");
+    this.h1_aciklama.style.color = colors.c_3;
+    ///// urun fiyat
+    this.h1_div_fiyat = CrateElement("div","","meno_h1_div");
+    this.h1_fiyat = CrateElement("h1",""+fiyat_+" $","meno_h1");
+    this.h1_fiyat.style.color = colors.c_3;
+
+    this.paszamine.appendChild(this.img);
+    this.paszamine.appendChild(this.h1_div);
+    this.h1_div.appendChild(this.h1);
+    
+    
+    this.paszamine.appendChild(this.h1_div_aciklama);
+    this.h1_div_aciklama.appendChild(this.h1_aciklama);
+    
+    this.paszamine.appendChild(this.h1_div_fiyat);
+    this.h1_div_fiyat.appendChild(this.h1_fiyat);
+
+    this.paszamine.addEventListener("click",()=> {
+        localStorage.setItem("urun_id",this.id);
+    })
+}
+function Urunler() {
+    this.paszamine = CrateElement("div","","menolar_paszamine");
+    this.meno = [];
+    urunler_data.forEach(e => {
+        if(e.meno_id == localStorage.getItem("meno_id")) {
+        this.meno.push(new Urun(e.id,e.meno_name,e.img,e.aciklama,e.fiyat,e.meno_id));
+        }
+    });
+    this.Crate();
+    this.meno.forEach(e => {
+    
+        e.h1_div.style.top = ((e.paszamine.getBoundingClientRect().height/10))+"px";
+        e.h1_div_aciklama.style.top = (((e.h1_div.getBoundingClientRect().y+e.h1_div.getBoundingClientRect().height)-e.paszamine.getBoundingClientRect().y)+20)+"px";
+        e.h1_div_fiyat.style.top = (((e.h1_div_aciklama.getBoundingClientRect().y+e.h1_div_aciklama.getBoundingClientRect().height)-e.paszamine.getBoundingClientRect().y)+20)+"px";
+        e.paszamine.style.height = (((e.h1_div_fiyat.getBoundingClientRect().y+e.h1_div_fiyat.getBoundingClientRect().height)-e.paszamine.getBoundingClientRect().y)+20)+"px";
+        
+    });
+    this.paszamine.style.cssText = " width: 100%;margin-top: "+(urun_ekle.ekle_icon.getBoundingClientRect().y+urun_ekle.ekle_icon.getBoundingClientRect().height)*1.3+"px;overflow-y: scroll;"
+    this.paszamine.style.height = innerHeight-this.paszamine.getBoundingClientRect().y+"px";
+    
+}
+Urunler.prototype.Crate = function() {
     paszamine_s.appendChild(this.paszamine);
     this.meno.forEach(element => {
         this.paszamine.appendChild(element.paszamine);
